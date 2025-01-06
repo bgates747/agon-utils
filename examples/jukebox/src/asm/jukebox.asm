f23d7000804010b2bbdbc0985ac575066f4e5d0f
bgates747 <brandon.gates@gmail.com>
2025-01-06 16:04:24 -0500

    input key debouncing works. fixed unbalanced call / returns by using jps. push pops replaced with variables for clarity and stack balance issues

--- examples/jukebox/src/asm/jukebox.asm
+++ examples/jukebox/src/asm/jukebox.asm
@@ -42,8 +42,9 @@ exit:
     include "vdu_sound.inc"
 
 ; APPLICATION INCLUDES
-    include "music.inc"
     include "input.inc"
+    include "music.inc"
+    include "play.inc"
     include "debug.inc"
 
 ; --- MAIN PROGRAM FILE ---
@@ -57,192 +58,14 @@ init:
     ret
 ; end init
 
-ch0_buffer: equ 0x3000
-ch1_buffer: equ 0x3001
-cmd0_buffer: equ 0x3002
-cmd1_buffer: equ 0x3003
-
 main:
     call printNewLine
 ; default song
     ld hl,FRHIANNON
-
-; stream a song from the SD card
-; inputs: hl = pointer to filename
-; requirements: the file must be 8-bit signed PCM mono sampled at 15360 Hz
-; uses: sound channels 0 and 1, buffers 0x3000 and 0x3001
-play_song:
-; tell the user what they've won
-    push hl
-    call printInline
-    asciz "Playing song: "
-    pop hl
-    push hl
-    call printString ; print the song filename
-    call printNewLine
-    pop hl
-
-; open the file in read mode
-; Open a file
-; HLU: Filename
-;   C: Mode
-; Returns:
-;   A: Filehandle, or 0 if couldn't open
-	ld c,fa_read
-    MOSCALL mos_fopen
-    ld (@filehandle),a
-
-@read_file:
-; set the play next sample timer
-    ld iy,tmr_play
-    ld hl,120 ; 1 second
-    call tmr_set
-
-; Read a block of data from a file
-;   C: Filehandle
-; HLU: Pointer to where to write the data to
-; DEU: Number of bytes to read
-; Returns:
-; DEU: Number of bytes read
-    ld a,(@filehandle)
-    ld c,a
-    ld hl,song_data
-    ld de,sample_rate
-    MOSCALL mos_fread
-
-; test de for zero bytes read
-    ld hl,0
-    xor a ; clear carry
-    sbc hl,de
-    jp z,@close_file
-
-; load a vdu buffer from local memory
-; inputs: hl = bufferId ; bc = length ; de = pointer to data
-    ld a,(@channel)
-    inc a
-    and 1
-    ld (@channel),a
-    ld hl,ch0_buffer
-    ld l,a
-    push hl ; sample bufferId
-    call vdu_clear_buffer
-    pop hl
-    push hl
-    push de ; chunksize
-    pop bc
-    ld de,song_data
-    call vdu_load_buffer
-
-; call the command buffer to play the sound
-    pop hl
-    inc l
-    inc l ; play commmand bufferId
-    call vdu_call_buffer
-
-; rcall user input loop
-    call get_input
-
-; read the next block
-    jp @read_file
-
-; close the file
-@close_file:
-    pop af ; dummy pop to balance stack
-    ld a,(@filehandle)
-    MOSCALL mos_fclose
+    call play_song
     ret
+; end main
 
-@channel: db 0 ; channel number
-
-@filehandle: db 0 ; file handle
-@fil: dl 0 ; pointer to FIL struct
-
-@chunkpointer: dl 0 ; pointer to current chunk
-
-; File information structure (FILINFO)
-@filinfo:
-@filinfo_fsize:    blkb 4, 0   ; File size (4 bytes)
-@filinfo_fdate:    blkb 2, 0   ; Modified date (2 bytes)
-@filinfo_ftime:    blkb 2, 0   ; Modified time (2 bytes)
-@filinfo_fattrib:  blkb 1, 0   ; File attribute (1 byte)
-@filinfo_altname:  blkb 13, 0  ; Alternative file name (13 bytes)
-@filinfo_fname:    blkb 256, 0 ; Primary file name (256 bytes)
-
-
-load_command_buffer:
-    ld hl,cmd0_buffer
-    call vdu_clear_buffer
-    ld hl,cmd0_buffer
-    ld bc,@cmd0_end-@cmd0
-    ld de,@cmd0
-    call vdu_write_block_to_buffer
-
-    ld hl,cmd1_buffer
-    call vdu_clear_buffer
-    ld hl,cmd1_buffer
-    ld bc,@cmd1_end-@cmd1
-    ld de,@cmd1
-    call vdu_write_block_to_buffer
-    ret
-
-@cmd0:
-; vdu_buffer_to_sound command string
-; Command 5: Buffer to sound
-; VDU 23, 0, &85, channel (ignored), 5, 2, bufferId; format, [sampleRate;]
-    db 23,0,0x85 ; vdu sound command header
-    db 0x00 ; channel (ignored)
-    db 0x05 ; buffer to sound command
-    db 0x02 ; command 2 create sample
-    dw ch0_buffer
-    db 0+8 ; 0 = 8-bit signed PCM mono, 8 = sample rate argument follows
-    dw sample_rate ; Hz
-; vdu_play_sfx command string
-; Command 4: Set waveform
-; VDU 23, 0, &85, channel, 4, waveformOrSample, [bufferId;]
-    db 23,0,$85 ; vdu sound command header  
-    db 0 ; channel
-    db 4 ; set waveform command
-    db 8 ; waveform 8 = sample
-    dw ch0_buffer ; sample bufferId
-; Command 0: Play note
-; VDU 23, 0, &85, channel, 0, volume, frequency; duration;
-    db 23,0,$85 ; vdu sound command header
-    db 0 ; channel
-    db 0 ; play note command
-    db 127  ; volume 127 = max
-    dw 0 ; frequency (relevant only for tuneable samples)
-    dw 0 ; duration (ms), zero means play one time in full
-@cmd0_end:
-
-@cmd1:
-; vdu_buffer_to_sound command string
-; Command 5: Buffer to sound
-; VDU 23, 0, &85, channel (ignored), 5, 2, bufferId; format, [sampleRate;]
-    db 23,0,0x85 ; vdu sound command header
-    db 0x00 ; channel (ignored)
-    db 0x05 ; buffer to sound command
-    db 0x02 ; command 2 create sample
-    dw ch1_buffer
-    db 0+8 ; 0 = 8-bit signed PCM mono, 8 = sample rate argument follows
-    dw sample_rate ; Hz
-; vdu_play_sfx command string
-; Command 4: Set waveform
-; VDU 23, 0, &85, channel, 4, waveformOrSample, [bufferId;]
-    db 23,0,$85 ; vdu sound command header  
-    db 1 ; channel
-    db 4 ; set waveform command
-    db 8 ; waveform 8 = sample
-    dw ch1_buffer ; sample bufferId
-; Command 0: Play note
-; VDU 23, 0, &85, channel, 0, volume, frequency; duration;
-    db 23,0,$85 ; vdu sound command header
-    db 1 ; channel
-    db 0 ; play note command
-    db 127  ; volume 127 = max
-    dw 0 ; frequency (relevant only for tuneable samples)
-    dw 0 ; duration (ms), zero means play one time in full
-@cmd1_end:
-; end load_command_buffers
-
-    align 256 ; align to 256-byte boundary (may be helpful ... or not)
-song_data: ; blkw 6556,0 ; buffer for sound data
+; buffer for sound data
+; (must be last so buffer doesn't overwrite other program code or data)
+song_data:
